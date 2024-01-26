7071a8b8efd26e18756c3da66ef054e7945825d2
Justin Helps <justin.r.helps@gmail.com>
2024-01-26 11:44:05 -0600

    Move timeline animation tool into PrimerTools repo.

---
+++ AnimationScripts/CombinedAnimation.cs
@@ -0,0 +1,41 @@
+using Godot;
+
+namespace TimelineTool;
+
+[Tool]
+public partial class CombinedAnimation : AnimationPlayer
+{
+	[Export]
+	private AnimationPlayer innerAnimationPlayer1;
+	[Export]
+	private AnimationPlayer innerAnimationPlayer2;
+
+	private double lastAnimationPosition = -1;
+
+	public override void _Process(double delta)
+	{
+		// GD.Print("Processssssssssssssssssssss");
+		if (Engine.IsEditorHint())
+		{
+			if (IsPlaying() || lastAnimationPosition != CurrentAnimationPosition)
+			{
+				OnSeeked(CurrentAnimationPosition);
+				lastAnimationPosition = CurrentAnimationPosition;
+			}
+		}
+	}
+
+	private void OnSeeked(double pos)
+	{
+		GD.Print("Seeked to position: ", pos);
+		// Synchronize inner animation players when scrubbed
+		SynchronizeInnerAnimationPlayers();
+	}
+
+	private void SynchronizeInnerAnimationPlayers()
+	{
+		var currentTime = CurrentAnimationPosition;
+		innerAnimationPlayer1.Seek(currentTime, true);
+		innerAnimationPlayer2.Seek(currentTime, true);
+	}
+}
