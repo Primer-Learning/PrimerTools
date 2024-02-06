7985f90d905d0c71595b1b284d5a9e617196bd70
Justin Helps <justin.r.helps@gmail.com>
2024-02-06 15:52:41 -0600

    Make AnimationSequence work in the running game. The issue was that the path to the animation was defined with an absolute path while in the editor. And editor paths are different.

--- AnimationSequence/AnimationSequence.cs
+++ AnimationSequence/AnimationSequence.cs
@@ -25,11 +25,10 @@ public abstract partial class AnimationSequence : AnimationPlayer
 	
 	public override void _Ready()
 	{
-		// This doesn't work!
-		// if (!Engine.IsEditorHint())
-		// {
-		// 	Play("p/CombinedAnimation");
-		// }
+		if (!Engine.IsEditorHint())
+		{
+			Play("p/CombinedAnimation");
+		}
 	}
 
 	protected abstract void Define();
@@ -39,8 +38,8 @@ public abstract partial class AnimationSequence : AnimationPlayer
 		{
 			child.QueueFree();
 		}
+		referenceAnimationPlayer ??= CreateReferenceAnimationPlayer();
 		
-		referenceAnimationPlayer = CreateReferenceAnimationPlayer();
 		// Reset the index for the library
 		animationsMade = 0;
 	}
@@ -48,23 +47,21 @@ public abstract partial class AnimationSequence : AnimationPlayer
 	#region Animation Methods
 	protected void MoveAnimation(Node3D node3D, Vector3 to, Vector3? from = null, float duration = 1f)
 	{
-		// node3D.Owner = GetTree().EditedSceneRoot;
-		
-		// referenceAnimationPlayer ??= CreateReferenceAnimationPlayer();
+		node3D.Owner = GetTree().EditedSceneRoot;
 		
 		// Make or get the AnimationLibrary
 		var library = MakeOrGetAnimationLibrary(referenceAnimationPlayer, "p");
 		
 		var animation = new Animation();
-		animation.Length = duration; // Set the length of the animation
+		animation.Length = duration;
 		var trackIndex = animation.AddTrack(Animation.TrackType.Value);
 		
 		// Put the library in the animation player
 		AddAnimationToLibrary(animation, $"anim{animationsMade}", library);
 		animationsMade++;
 		
-		animation.TrackSetPath(trackIndex, $"{node3D.GetPath()}:position");
-		animation.TrackInsertKey(trackIndex, 0.0f, from.HasValue ? from.Value : node3D.Position);
+		animation.TrackSetPath(trackIndex, $"{Name}/{node3D.Name}:position");
+		animation.TrackInsertKey(trackIndex, 0.0f, from ?? node3D.Position);
 		animation.TrackInsertKey(trackIndex, 1.0f, to);
 		node3D.Position = to;
 	}
@@ -78,7 +75,9 @@ public abstract partial class AnimationSequence : AnimationPlayer
 		
 		// TODO: Make a dictionary of animations and start times
 		// Start times can be gotten from the top-level animation player if they exist already
-		animation.TrackSetPath(trackIndex, $"{referenceAnimationPlayer.GetPath()}:animation");
+		animation.TrackSetPath(trackIndex, $"ReferenceAnimationPlayer:animation");
+		GD.Print("Current Scene: " + GetTree().CurrentScene);
+		GD.Print("Edited Scene Root: " + GetTree().EditedSceneRoot.Name);
 		
 		// TODO: Make time the minimum of next start time and previous end time
 		var time = 0.0f;
@@ -99,8 +98,9 @@ public abstract partial class AnimationSequence : AnimationPlayer
 		// It's necessary because an animation playback track needs to reference an AnimationPlayer, not just a library
 		referenceAnimationPlayer?.QueueFree();
 		var newPlayer = new AnimationPlayer();
-		AddChild(newPlayer);
-		newPlayer.Owner = GetTree().EditedSceneRoot;
+		newPlayer.Name = "ReferenceAnimationPlayer";
+		GetParent().AddChild(newPlayer);
+		newPlayer.Owner = GetParent();
 		return newPlayer;
 	}
 	
