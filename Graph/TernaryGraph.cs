3a728113222ad28c4328410554211f6091dd4245
Justin Helps <justin.r.helps@gmail.com>
2024-02-23 11:56:44 -0600

    Make TernaryGraph its own file.

---
+++ Graph/TernaryGraph.cs
@@ -0,0 +1,72 @@
+﻿using System.Linq;
+using Godot;
+
+namespace PrimerTools.Graph;
+
+public partial class TernaryGraph : Node3D
+{
+    public void CreateBounds(float chonk = 0.01f)
+    {
+        var corners = new Vector3[]
+        {
+            new (0, 0, 0),
+            new (1, 0, 0),
+            new (0.5f, Mathf.Sqrt(3) / 2, 0)
+        };
+
+        for (var i = 0; i < 3; i++)
+        {
+            // Cylinder
+            var cylinder = new MeshInstance3D();
+            cylinder.Name = $"Cylinder {i}";
+            AddChild(cylinder);
+            cylinder.Owner = GetTree().EditedSceneRoot;
+
+            var mesh = new CylinderMesh();
+            mesh.Height = 1;
+            mesh.BottomRadius = chonk;
+            mesh.TopRadius = chonk;
+            cylinder.Mesh = mesh;
+            
+            cylinder.Position = (corners[i] + corners[(i + 1) % 3]) / 2;
+            cylinder.RotationDegrees = new Vector3(0, 0, 90 + 120 * i);
+            
+            // Sphere
+            var sphere = new MeshInstance3D();
+            sphere.Name = $"Sphere {i}";
+            AddChild(sphere);
+            sphere.Owner = GetTree().EditedSceneRoot;
+            
+            var sMesh = new SphereMesh();
+            sMesh.Height = 2 * chonk;
+            sMesh.Radius = chonk;
+            sphere.Mesh = sMesh;
+            
+            sphere.Position = corners[i];
+        }
+
+
+    }
+    
+    public static Vector3 CoordinatesToPosition(float a, float b, float c)
+    {
+        ValidateCoordinates(a, b, c);
+
+        return new Vector3(
+            b + c / 2,
+            c * Mathf.Sqrt(3) / 2,
+            0
+        );
+    }
+
+    private static void ValidateCoordinates(params float[] coords)
+    {
+        var sum = coords.Sum();
+
+        if (Mathf.Abs(sum - 1) > 0.00001f)
+            GD.PushWarning($"Sum of coordinates must be 1 but it's {sum}");
+
+        if (coords.Any(x => x < 0))
+            GD.PushWarning($"Coordinates must all be positive, but they are {coords.Join()}");
+    }
+}
