4c128d9ca74838d09126f2d5560c60545edfe693
Justin Helps <justin.r.helps@gmail.com>
2024-03-15 15:13:26 -0500

    Add FPS viewer

---
+++ Utilities/FPSViewer.cs
@@ -0,0 +1,16 @@
+﻿using Godot;
+
+namespace PrimerTools;
+
+public partial class FPSViewer : Label
+{
+    public override void _Ready()
+    {
+        Engine.MaxFps = 0;
+    }
+
+    public override void _Process(double delta)
+    {
+        Text = $"{Engine.GetFramesPerSecond()} FPS";
+    }
+}
