16c22b97f1be7189844e36b3556c65b2d248ddfb
Justin Helps <justin.r.helps@gmail.com>
2024-05-01 18:18:16 -0500

    Make most of a ternary bar graph. Still need to fix labels and maybe some other stuff.

---
+++ Graph/TernaryGraphWithBars.cs
@@ -0,0 +1,66 @@
+﻿using System.Linq;
+using Godot;
+using PrimerAssets;
+
+namespace PrimerTools.Graph;
+
+public partial class TernaryGraphWithBars : TernaryGraph
+{
+    [Export] public int BarsPerSide = 10;
+
+    private PackedScene hexagonalPrismScene =
+        ResourceLoader.Load<PackedScene>("res://addons/PrimerTools/Graph/hexagonal_prism.tscn");
+
+    public float[] Data;
+    
+    public void AddBars()
+    {
+        var increment = 1f / (BarsPerSide - 1);
+        var k = 0;
+        for (var i = 0; i <= BarsPerSide; i++)
+        {
+            for (var j = 0; j < BarsPerSide; j++)
+            {
+                if (j + i >= BarsPerSide) continue;
+                
+                var bar = MakeBar();
+                AddChild(bar);
+                
+                var a = (BarsPerSide - 1 - i - j) * increment ;
+                var b = i * increment;
+                var c = j * increment;
+                
+                bar.Position = CoordinatesToPosition(a, b, c);
+
+                var mat = new StandardMaterial3D();
+                mat.AlbedoColor = PrimerColor.MixColorsByWeight(
+                    colors: Colors.ToArray(),
+                    new []{ a, b, c },
+                    subtractive: true,
+                    messWithBrightness: true
+                );
+
+                bar.GetNode<MeshInstance3D>("Cylinder").Mesh.SurfaceSetMaterial(0, mat); 
+                bar.RotationDegrees = new Vector3(90, 0, 0);
+                
+                // We need to fit 1 less than the number of bars into the range from 0 to 1
+                // Also, 1 is the default diameter from corner to corner in the cylinder mesh, 
+                // So need to do the sqrt 3 thing to make the sides touch.
+                var widthScale = 1f / (BarsPerSide - 1) / (Mathf.Sqrt(3) / 2) / 2;
+                bar.Scale = new Vector3(widthScale, Data[k], widthScale);
+
+                k++;
+            }
+        }
+    }
+    
+    private Node3D MakeBar()
+    {
+        var bar = hexagonalPrismScene.Instantiate<Node3D>();
+        var meshInstance3D = bar.GetNode<MeshInstance3D>("Cylinder");
+        var newMesh = (Mesh)bar.GetNode<MeshInstance3D>("Cylinder").Mesh.Duplicate();
+        meshInstance3D.Mesh = newMesh;
+
+        return bar;
+    }
+}
---
+++ TestScenes/Graph/TernaryGraphTestSequence.cs
@@ -0,0 +1,39 @@
+using Godot;
+using PrimerAssets;
+using PrimerTools;
+using PrimerTools.AnimationSequence;
+using PrimerTools.Graph;
+
+[Tool]
+public partial class TernaryGraphTestSequence : AnimationSequence
+{
+	protected override void Define()
+	{
+		var ternaryPlot = new TernaryGraphWithBars();
+		ternaryPlot.Colors = new[]
+		{
+			PrimerColor.red,
+			PrimerColor.blue,
+			PrimerColor.yellow
+		};
+		AddChild(ternaryPlot);
+		ternaryPlot.CreateBounds();
+
+		RegisterAnimation(ternaryPlot.ScaleTo(1));
+
+		ternaryPlot.BarsPerSide = 11;
+		
+		var numBars = ternaryPlot.BarsPerSide * (ternaryPlot.BarsPerSide + 1) / 2;
+		var data = new float[numBars];
+		var rng = new System.Random(23);
+		for (var i = 0; i < numBars; i++)
+		{
+			data[i] = (float)rng.NextDouble();
+		}
+
+		ternaryPlot.Data = data;
+		
+		ternaryPlot.AddBars();
+		ternaryPlot.MakeSelfAndChildrenLocal(GetTree().EditedSceneRoot);
+	}
+}
