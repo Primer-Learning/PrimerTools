a88d403c81d88f38bff630071d53d3514ba7272d
Justin Helps <justin.r.helps@gmail.com>
2024-05-10 10:52:04 -0500

    Add PrimerGD, which for now just has a shortcut to printing with a stack trace.

---
+++ Utilities/PrimerGD.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using Godot;
+
+namespace PrimerTools;
+
+public static class PrimerGD
+{
+    public static void PrintWithStackTrace(params object[] what)
+    {
+        var stackTrace = new System.Diagnostics.StackTrace(true);
+        var whatWithStackTrace = new List<object>();
+        whatWithStackTrace.AddRange(what);
+        whatWithStackTrace.Add("\n" + stackTrace);
+        GD.Print(whatWithStackTrace.ToArray());
+    }
+}
