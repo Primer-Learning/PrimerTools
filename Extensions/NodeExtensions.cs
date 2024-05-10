ee3456fc3eb4c15487bd270c7d14bc7c9dffef78
Justin Helps <justin.r.helps@gmail.com>
2024-05-10 12:58:46 -0500

    Make the MakeLocal method work without arguments

--- Extensions/NodeExtensions.cs
+++ Extensions/NodeExtensions.cs
@@ -6,8 +6,13 @@ public static class NodeExtensions
 {
     // This is for scenes that instantiate packed scenes, if you want to edit the children in the editor
     // It breaks inheritance, so use with caution. Or abandon! I'm not the boss of you.
-    public static void MakeSelfAndChildrenLocal(this Node parent, Node ancestorWhoNodesAreLocalWithRespectTo, int depth = 0)
+    public static void MakeSelfAndChildrenLocal(this Node parent, Node ancestorWhoNodesAreLocalWithRespectTo = null, int depth = 0)
     {
+        if (ancestorWhoNodesAreLocalWithRespectTo == null)
+        {
+            ancestorWhoNodesAreLocalWithRespectTo = parent.GetTree().EditedSceneRoot;
+        }
+        
         parent.Owner = ancestorWhoNodesAreLocalWithRespectTo;
         parent.SceneFilePath = "";
         if (depth > 100)
