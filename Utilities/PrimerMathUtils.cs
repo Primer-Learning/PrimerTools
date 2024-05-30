419f05c901d08b0772a33973e2aa556cdc39e1ea
Justin Helps <justin.r.helps@gmail.com>
2024-05-30 09:55:21 -0500

    Add PrimerMathUtils and a decaying exponential with min method.

---
+++ Utilities/PrimerMathUtils.cs
@@ -0,0 +1,12 @@
+﻿using Godot;
+
+namespace PrimerTools;
+
+public static class PrimerMathUtils
+{
+    public static float DecayingExponentialWithMin(float initialDuration, float decayFactor, float minDuration, float step)
+    {
+        // TODO: Could fit this to an actual exponential instead of clamping, but who cares
+        return Mathf.Max(initialDuration * Mathf.Pow(decayFactor, step), minDuration);
+    }
+}
