75f77acc61870fcce8267a339ecc5c41c78c1a0a
Helpsypoo <justin.r.helps@gmail.com>
2024-08-15 17:46:33 -0500

    Make self building blod animation tree more complete. It now makes two state machines. One for the body, and one for the mouth. And it filters their contents based on animation prefix.

--- Animation/BlobAnimationTree.cs
+++ Animation/BlobAnimationTree.cs
@@ -8,30 +8,73 @@ using PrimerTools;
 public partial class BlobAnimationTree : SelfBuildingAnimationTree
 {
 	private int _circleRadius = 300;
+
+	private Vector2 CalculatePosition(int index, int total)
+	{
+		// Starts at 180 degrees so the "first" thing is on the left.
+		var angle = 2 * Mathf.Pi / total * index - Mathf.Pi;
+		return _circleRadius * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle));
+	}
 	
 	protected override void Build()
 	{
 		base.Build();
 
+		var topLevelBlendTree = new AnimationNodeBlendTree();
+
+		// Make the BODY state machine
+		var bodyStateMachine = CreateOmniconnectedStateMachine("BODY", true);
+		// Make the MOUTH state machine
+		var mouthStateMachine = CreateOmniconnectedStateMachine("MOUTH", false);
+		
+		// Unsure whether I need to clear the old state machine. Probably not?
+		topLevelBlendTree.AddNode("BODY", bodyStateMachine);
+		topLevelBlendTree.SetNodePosition("BODY", new Vector2(-400, -300));
+		topLevelBlendTree.AddNode("MOUTH", mouthStateMachine);
+		topLevelBlendTree.SetNodePosition("MOUTH", new Vector2(-200, 200));
+
+		var addNode = new AnimationNodeAdd2();
+		Set("parameters/Add/add_amount", 1);
+		topLevelBlendTree.AddNode("Add", addNode);
+		
+		topLevelBlendTree.ConnectNode("Add", 0, "BODY");
+		topLevelBlendTree.ConnectNode("Add", 1, "MOUTH");
+		topLevelBlendTree.ConnectNode("output", 0, "Add");
+		
+		TreeRoot = topLevelBlendTree;
+	}
+
+	private AnimationNodeStateMachine CreateOmniconnectedStateMachine(string prefix, bool loadWiggles)
+	{
 		var stateMachine = new AnimationNodeStateMachine();
 		stateMachine.SetNodePosition("Start", new Vector2(- 2 * _circleRadius, 0));
 		stateMachine.SetGraphOffset(new Vector2(- 2 * _circleRadius - 100, -250));
 		
-		var allAnimations = GetNode<AnimationPlayer>(AnimPlayer).GetAnimationList();
+		var bodyAnimations = GetNode<AnimationPlayer>(AnimPlayer).GetAnimationList().Where(x => x.StartsWith(prefix)).ToList();
 		var animationNodesSoFar = new List<StringName>();
 		
-		foreach (var (animName, index) in allAnimations.WithIndex())
+		// Add the basic animations
+		foreach (var animName in bodyAnimations)
 		{
 			var animationNode = new AnimationNodeAnimation();
 			animationNode.Animation = animName;
-			GD.Print(stateMachine.GetNodePosition(animName));
 			stateMachine.AddNode(animName, animationNode);
-			
+		}
+		
+		// Add wiggles blend tree if needed
+		if (loadWiggles)
+		{
+			// Set up the wiggles state, which is more complex and won't change often, so it's loaded rather than generated
+			var wigglesTree = ResourceLoader.Load<AnimationNodeBlendTree>("res://addons/PrimerAssets/Organized/Blob/Blobs/blob_wiggle_tree.tres");
+			stateMachine.AddNode("Wiggles", wigglesTree, CalculatePosition(0, bodyAnimations.Count + 1));
+			bodyAnimations.Insert(0, "Wiggles");
+		}
+		
+		foreach (var (animName, index) in bodyAnimations.WithIndex())
+		{
 			// Set position
-			var angle = 2 * Mathf.Pi / allAnimations.Length * index - Mathf.Pi;
-			stateMachine.SetNodePosition(animName, _circleRadius * new Vector2(Mathf.Cos(angle), Mathf.Sin(angle)));
-			
-			// If it's the first one, connect it to start
+			stateMachine.SetNodePosition(animName, CalculatePosition(index, bodyAnimations.Count)); // +1 is for the wiggles, which is loaded before this loop 
+
 			if (animationNodesSoFar.Count == 0)
 			{
 				var startTransition = new AnimationNodeStateMachineTransition();
@@ -39,8 +82,9 @@ public partial class BlobAnimationTree : SelfBuildingAnimationTree
 				startTransition.XfadeTime = 0.25f;
 				startTransition.AdvanceCondition = animName;
 				stateMachine.AddTransition("Start", animName, startTransition);
+				Set($"parameters/{prefix}/conditions/{animName}", true);
 			}
-
+			
 			// Connect to previously placed nodes
 			foreach (var existingNodeName in animationNodesSoFar)
 			{
@@ -60,7 +104,6 @@ public partial class BlobAnimationTree : SelfBuildingAnimationTree
 			animationNodesSoFar.Add(animName);
 		}
 		
-		// Unsure whether I need to clear the old state machine. Probably not?
-		TreeRoot = stateMachine;
+		return stateMachine;
 	}
 }
