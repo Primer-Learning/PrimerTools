7c404e1debba417e812350c3724608cb0b36d52a
Helpsypoo <justin.r.helps@gmail.com>
2024-08-29 12:43:37 -0500

    Make PeriodicPlotter as a node meant to trigger periodic plotting in a CurvePlot2D

---
+++ Simulation/DataVisualization/PeriodicPlotter.cs
@@ -0,0 +1,26 @@
+﻿using Godot;
+using PrimerTools.Graph;
+
+public partial class PeriodicPlotter : Node
+{
+    public CurvePlot2D Curve;
+
+    [Export] public bool Plotting = true;
+    private double _timeSinceLastPlot;
+    public override void _Process(double delta)
+    {
+        if (!Plotting) return;
+        _timeSinceLastPlot += delta;
+        if (_timeSinceLastPlot < 1) return;
+        PlotData();
+        _timeSinceLastPlot = 0;
+    }
+    
+    private void PlotData()
+    {
+        var numPoints = Curve.GetData().Length;
+        Curve.FetchData();
+        if (numPoints == Curve.GetData().Length) return;
+        Curve.TweenTransition(1);
+    }
+}
