5866e0cf47545f648c14afc449d911e1646414f5
Helpsypoo <justin.r.helps@gmail.com>
2024-10-28 11:16:54 -0500

    Refactor BarData3DUtilities to use options instead of many overloads.

--- Graph/DataObjects/BarPlot3D/BarData3DUtilities.cs
+++ Graph/DataObjects/BarPlot3D/BarData3DUtilities.cs
@@ -5,9 +5,16 @@ using Godot;
 
 namespace PrimerTools.Graph;
 
+public class Histogram2DOptions
+{
+    public bool Normalize { get; set; }
+    public float BinWidthX { get; set; } = 1;
+    public float BinWidthY { get; set; } = 1;
+}
+
 public static class BarData3DUtilities
 {
-    public static float[,] MakeHistogram2D(IEnumerable<(float x, float y)> dataToBin, float binWidthX, float binWidthY)
+    private static float[,] MakeHistogram2D(IEnumerable<(float x, float y)> dataToBin, Histogram2DOptions options)
     {
         var toBin = dataToBin.ToArray();
         if (!toBin.Any()) return new float[0, 0];
@@ -15,8 +22,8 @@ public static class BarData3DUtilities
         var maxX = toBin.Max(point => point.x);
         var maxY = toBin.Max(point => point.y);
         
-        var numBinsX = Mathf.CeilToInt(maxX / binWidthX);
-        var numBinsY = Mathf.CeilToInt(maxY / binWidthY);
+        var numBinsX = Mathf.CeilToInt(maxX / options.BinWidthX);
+        var numBinsY = Mathf.CeilToInt(maxY / options.BinWidthY);
         var histogram = new float[numBinsX, numBinsY];
 
         foreach (var (x, y) in toBin)
@@ -28,94 +35,62 @@ public static class BarData3DUtilities
                 continue;
             }
 
-            var binIndexX = Mathf.CeilToInt(x / binWidthX) - 1;
-            var binIndexY = Mathf.CeilToInt(y / binWidthY) - 1;
+            var binIndexX = Mathf.CeilToInt(x / options.BinWidthX) - 1;
+            var binIndexY = Mathf.CeilToInt(y / options.BinWidthY) - 1;
             histogram[binIndexX, binIndexY]++;
         }
 
         return histogram;
     }
 
-    public static BarPlot3D.DataFetch PropertyHistogram2D<T>(
-        Func<IEnumerable<T>> dataSourceGetter, 
-        Func<T, (float x, float y)> propertySelector,
-        float binWidthX = 1,
-        float binWidthY = 1)
+    private static float[,] NormalizeHistogram(float[,] histogram)
     {
-        return () => MakeHistogram2D(
-            dataSourceGetter().Select(propertySelector),
-            binWidthX,
-            binWidthY);
+        var sum = 0f;
+        for (var x = 0; x < histogram.GetLength(0); x++)
+        for (var y = 0; y < histogram.GetLength(1); y++)
+            sum += histogram[x, y];
+
+        if (sum == 0) return histogram;
+            
+        var normalized = new float[histogram.GetLength(0), histogram.GetLength(1)];
+        for (var x = 0; x < histogram.GetLength(0); x++)
+        for (var y = 0; y < histogram.GetLength(1); y++)
+            normalized[x, y] = histogram[x, y] / sum;
+
+        return normalized;
     }
 
-    public static BarPlot3D.DataFetch NormalizedPropertyHistogram2D<T>(
+    public static BarPlot3D.DataFetch PropertyHistogram2D<T>(
         Func<IEnumerable<T>> dataSourceGetter,
         Func<T, (float x, float y)> propertySelector,
-        float binWidthX = 1,
-        float binWidthY = 1)
+        Histogram2DOptions options = null)
     {
+        options ??= new Histogram2DOptions();
+        
         return () =>
         {
             var histogram = MakeHistogram2D(
                 dataSourceGetter().Select(propertySelector),
-                binWidthX,
-                binWidthY);
-            
-            var sum = 0f;
-            for (var x = 0; x < histogram.GetLength(0); x++)
-            for (var y = 0; y < histogram.GetLength(1); y++)
-                sum += histogram[x, y];
+                options);
 
-            if (sum == 0) return histogram;
-            
-            var normalized = new float[histogram.GetLength(0), histogram.GetLength(1)];
-            for (var x = 0; x < histogram.GetLength(0); x++)
-            for (var y = 0; y < histogram.GetLength(1); y++)
-                normalized[x, y] = histogram[x, y] / sum;
-
-            return normalized;
+            return options.Normalize ? NormalizeHistogram(histogram) : histogram;
         };
     }
 
     public static BarPlot3D.DataFetch PropertyHistogram2D<T>(
         Func<IEnumerable<T>> dataSourceGetter,
         Func<T, IEnumerable<(float x, float y)>> propertySelector,
-        float binWidthX = 1,
-        float binWidthY = 1)
+        Histogram2DOptions options = null)
     {
-        return () => MakeHistogram2D(
-            dataSourceGetter().SelectMany(propertySelector),
-            binWidthX,
-            binWidthY
-        );
-    }
-
-    public static BarPlot3D.DataFetch NormalizedPropertyHistogram2DMulti<T>(
-        Func<IEnumerable<T>> dataSourceGetter,
-        Func<T, IEnumerable<(float x, float y)>> propertySelector,
-        float binWidthX = 1,
-        float binWidthY = 1)
-    {
+        options ??= new Histogram2DOptions();
+        
         return () =>
         {
             var histogram = MakeHistogram2D(
                 dataSourceGetter().SelectMany(propertySelector),
-                binWidthX,
-                binWidthY);
-            
-            var sum = 0f;
-            for (var x = 0; x < histogram.GetLength(0); x++)
-            for (var y = 0; y < histogram.GetLength(1); y++)
-                sum += histogram[x, y];
+                options);
 
-            if (sum == 0) return histogram;
-            
-            var normalized = new float[histogram.GetLength(0), histogram.GetLength(1)];
-            for (var x = 0; x < histogram.GetLength(0); x++)
-            for (var y = 0; y < histogram.GetLength(1); y++)
-                normalized[x, y] = histogram[x, y] / sum;
-
-            return normalized;
+            return options.Normalize ? NormalizeHistogram(histogram) : histogram;
         };
     }
 }
