338a77de9863061c1a4aef7e370198ad84ef55f0
Justin Helps <justin.r.helps@gmail.com>
2025-02-11 19:58:49 -0600

    Make NodeEntity abstract.

--- Simulation/ContinuousSpaceTimeSims/General/Visual/NodeEntity.cs
+++ Simulation/ContinuousSpaceTimeSims/General/Visual/NodeEntity.cs
@@ -2,7 +2,7 @@ using Godot;
 
 namespace PrimerTools.Simulation;
 
-public partial class NodeEntity : Node3D
+public abstract partial class NodeEntity : Node3D
 {
     public virtual void Initialize(IDataEntity dataEntity){}
     public virtual void Update(IDataEntity dataEntity){}
